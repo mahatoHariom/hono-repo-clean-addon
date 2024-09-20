36e9bcc8d2636b38d66210b1f763ffbb40cd9561
naswa <endabelyuproject@gmail.com>
2024-09-20 22:44:58 +0700

    feat(index): add  route for openApi swagger, and productRoute

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { productRoute } from "./routes/product.route";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.notFound((c) => {
+  return c.text("Sorry, the page you are looking for does not exist.", 404);
+});
+app.onError((err, c) => {
+  return c.text(
+    "Oops! Something went wrong on our end. Please try again later.",
+    500,
+  );
+});
 
-export default app
+app.get("/api", swaggerUI({ url: "/doc" }));
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Nakama API",
+    description:
+      "Nakama API is a RESTful service that provides access to detailed data for one-piece premium merchandise online store",
+  },
+});
+
+// API ROUTES
+app.route("/products", productRoute);
+
+export default {
+  port: 3100,
+  fetch: app.fetch,
+};
