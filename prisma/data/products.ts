331d3e40fe99f7f514d59bb2a725e9cb80dd6713
naswa <endabelyuproject@gmail.com>
2024-09-20 22:46:28 +0700

    feat(product): add poduct route for getAll product, getProduct by id, getProduct by slug, update product by id, and delete product by id

---
+++ prisma/data/products.ts
@@ -0,0 +1,69 @@
+export const dataProducts = [
+  {
+    name: "Ace Neclacke",
+    slug: "ace-necklace",
+    price: 120000,
+    imageURL:
+      "https://onepiece.b-cdn.net/wp-content/uploads/2021/05/product-image-715658296.jpg",
+    description: "Favourite necklace of Portgas D. Ace",
+    category: "necklace",
+    stock: 5,
+    sku: "NCK-ACE-",
+  },
+  {
+    name: "Luffy Hat",
+    slug: "luffy-hat",
+    price: 250000,
+    imageURL:
+      "https://onepiece.b-cdn.net/wp-content/uploads/2023/05/Boy-Girl-One-Piece-Cap-Straw-Hat-Neck-String-Luffy-Flat-Hats-Cosplay-Japanese-Cartoon-Props-1.jpg",
+    description: "Favourite hat of Monkey D. Luffy",
+    category: "hat",
+    stock: 2,
+    sku: "HAT-LUFFY",
+  },
+  {
+    name: "Cosplay costume Luffy Gear 5 Nika",
+    slug: "cosplay-costume-luffy-gear-5-nika",
+    price: 800000,
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2023/09/Anime-Straw-Hat-Boy-Luffy-Cosplay-Costume-Gear-5-Nika-Luffy-Cosplay-Clothes-Kimono-Set-Christmas.jpg_640x640.jpg",
+    description: "Cosplay costume of Monkey D. Luffy when gear 5 (NIKA God)",
+    category: "cloth",
+    stock: 4,
+    sku: "CPL-LUFFY",
+  },
+  {
+    name: "Action Figure Luffy",
+    slug: "action-figure-luffy",
+    price: 360000,
+    imageURL:
+      "https://onepiece.b-cdn.net/wp-content/uploads/2022/01/17CM-Anime-One-Piece-Action-Figure-PVC-Luffy-New-Action-Collectible-Model-Decorations-Doll-Children-Toys.jpg",
+    description: "Action Figure of Monkey D. Luffy",
+    category: "action-figure",
+    stock: 7,
+    sku: "AF-LUFFY",
+  },
+  {
+    name: "Pencil Holder Luffy",
+    slug: "pencil-holder-luffy",
+    price: 200000,
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    description: "Quality: Resin Height: 9.5cm-10cm",
+    category: "hat",
+    stock: 5,
+    sku: "HLD-LUFFY",
+  },
+  {
+    name: "Roronoa Zoro Sandai Kitetsu 1045 Carbon Steel Sword",
+    slug: "zoro-kitetsu-1045-carbon-steel-sword",
+    price: 120000,
+    imageURL:
+      "https://onepiece.b-cdn.net/wp-content/uploads/2021/09/one-piece-sword-7.jpg",
+    description:
+      "actual samurai sword, 1045 carbon metal, handmade, full tang, sharpened, battle prepared, alloy fittings",
+    category: "sword",
+    stock: 5,
+    sku: "SWD-LUFFY",
+  },
+];
