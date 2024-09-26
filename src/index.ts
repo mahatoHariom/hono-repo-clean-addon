7f76197e44da9ac19564f8ffeff8290394812228
naswa <endabelyuproject@gmail.com>
2024-09-26 11:19:09 +0700

    feat(index): add logger and cors ( for specific origin)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { productRoute } from "./routes/product.route";
-
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 const app = new OpenAPIHono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Hello This is Nakama's API!");
 });
+app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin:
+      "'http://localhost:5173','https://nakama.endabelyu.store','https://nakama-api.endabelyu.store','https://postman-echo.com'",
+  }),
+);
 app.notFound((c) => {
   return c.text("Sorry, the page you are looking for does not exist.", 404);
 });
