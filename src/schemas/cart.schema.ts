d71c35dbe9d32679e67c3a6a849d4fd175c95124
naswa <endabelyuproject@gmail.com>
2024-10-05 20:01:55 +0700

    feat(cart.schema): add new scchema for cartItemSchema, cartPayloadSchema

--- src/schemas/cart.schema.ts
+++ src/schemas/cart.schema.ts
@@ -1,15 +1,20 @@
 import { z } from "@hono/zod-openapi";
+import { userSchema } from "./user.schema";
+import { productSchema } from "./product.schema";
+
+export const cartItemSchema = z.object({
+  id: z.string().cuid(), // Prisma uses cuid() for id generation
+  productId: z.string(),
+  quantity: z.number().min(1).default(1),
+  cartId: z.string(),
+  createdAt: z.date().default(new Date()), // For now(), representing current date
+  updatedAt: z.date(), // @updatedAt will be handled automatically by Prisma
+  product: productSchema.omit({ sku: true, description: true }),
+});
 
 export const cartSchema = z.object({
   id: z.string().openapi({ example: "cm1ajuml30000356vwniwvrxm" }),
   userId: z.string().openapi({ example: "um1ajuml30000356vwniwvrxm" }),
-  user: z.string().openapi({
-    example: "Favourite necklace of Portgas D. Ace",
-  }),
-  items: z.string().openapi({
-    example: "Favourite necklace of Portgas D. Ace",
-  }),
-
   createdAt: z.date().optional().openapi({
     description: "Timestamp when the data product was created",
     example: "2023-08-24T12:00:00Z",
@@ -45,8 +50,9 @@ type cartIdSchema = z.infer<typeof cartIdSchema>;
 // });
 // type cartSlugSchema = z.infer<typeof cartSlugSchema>;
 
-export const cartPayloadSchema = cartSchema.omit({
-  id: true,
-  createdAt: true,
-  updatedAt: true,
+export const cartPayloadSchema = z.object({
+  productId: z.string().openapi({ example: "cm1sl792000009bg8q1mfg79h" }),
+  quantity: z.number().positive().min(1).openapi({ example: 1 }),
 });
+
+export const cartIdPayloadSchema = z.string().min(1);
